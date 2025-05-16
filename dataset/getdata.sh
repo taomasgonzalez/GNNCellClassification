80a44dfc21e0518bfa4fb9e5d0760cf589158657
Tomas Gonzalez <24421759+taomasgonzalez@users.noreply.github.com>
2025-05-16 22:29:50 +0100

    DataAnalysis: add notebook to display data when processing it

--- dataset/getdata.sh
+++ dataset/getdata.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-mkdir -p data images
-cd data
-wget https://figshare.com/ndownloader/articles/22004273/versions/2 -O 22004273.zip
-unzip 22004273.zip
-cd ../images
+pushd "$( dirname "$0" )"
 
-start_url="https://spatial-dlpfc.s3.us-east-2.amazonaws.com/images"
+data_dir="data"
+img_dir="images"
 
-filenames="151676 151669 151507 151508 151672 151670 151673 151675 151510 151671 151674 151509"
-for id in $filenames; do
-    url="${start_url}/${id}_full_image.tif"
-    wget "$url" -O "${id}.tif"
-done
-cd -
+if [[ -d ${data_dir} && -d ${img_dir} ]]; then
+  echo "Don't download data: Both ${data_dir} and ${img_dir} exists"
+else
+  echo "Downloading data from source..."
+  mkdir -p data images
+  cd data
+  wget https://figshare.com/ndownloader/articles/22004273/versions/2 -O 22004273.zip
+  unzip 22004273.zip
+  cd ../images
+
+  start_url="https://spatial-dlpfc.s3.us-east-2.amazonaws.com/images"
+
+  filenames="151676 151669 151507 151508 151672 151670 151673 151675 151510 151671 151674 151509"
+  for id in $filenames; do
+      url="${start_url}/${id}_full_image.tif"
+      wget "$url" -O "${id}.tif"
+  done
+fi
+
+popd
